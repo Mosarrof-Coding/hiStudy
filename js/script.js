9e7e8ecd134e812cd1365b7186d739bae40dd879
Mosarrof-Coding <tinaberry112188@gmail.com>
2023-04-16 02:27:43 +0600

    added all file and folder

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// let school = Array.from(document.querySelectorAll('.school'));
+// school.map((item, index)=>{
+//     item.classList.add('active');
+//     setInterval(() => {
+//         school[index].classList.remove('active');
+//     }, 2000);
+// })
+

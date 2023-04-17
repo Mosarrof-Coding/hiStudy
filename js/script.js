82608c08015cb64d623e448251cff903620ebfa2
Mosarrof-Coding <tinaberry112188@gmail.com>
2023-04-17 21:34:48 +0600

    committed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 // let school = Array.from(document.querySelectorAll('.school'));
 // school.map((item, index)=>{
-//     item.classList.add('active');
+//     item.addEventListener('onload', (nin)=>{
+//         setInterval(() => {
+//             nin.classList.remove('active');
+//         }, 2000);
+//     })
 //     setInterval(() => {
-//         school[index].classList.remove('active');
+//         school[index].classList.add('active');
 //     }, 2000);
 // })
 
+
+// demos mixitup-------------
+$(function(){
+    $('.mixitup').mixItUp({ 
+        selectors: {
+          target: '.tile',
+          filter: '.filter',
+          sort: '.sort-btn'
+        },
+            animation: {
+            animateResizeContainer: false,
+            effects: 'fade scale'
+        }
+    });
+})
